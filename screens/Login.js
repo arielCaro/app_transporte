84081124d2fe8fbf8b97b29c3a25754be7cb8c06
arielCaro <arielCaro>
2024-09-09 16:53:02 -0300

    update

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,154 @@
-import  { View, Text } from 'react-native';
-import React from 'react';
+import  { View, Text, TextInput, Image, Pressable, StatusBar, ImageBackground } from 'react-native';
+import React , {useState} from 'react';
+import {SafeAreaView } from 'react-native-safe-area-context'
+import COLORS from '../constants/colors';
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from "@expo/vector-icons"; 
+import Checkbox from 'expo-checkbox'; 
+import Button from '../components/Button';
 
-const Login = () => {
+const Login = ({navigation}) => {
+    const [isPasswordShow, setIsPasswordShow] = useState(false);
+    const [isChecked, setIsChecked] = useState(false);
     return (
-        <View>
-            <Text> Hola Login</Text>
+      <SafeAreaView style={{flex:1, backgroundColor: COLORS.white}}>
+        <StatusBar />
+        <ImageBackground source={require('../assets/camion-coronel.jpeg')} 
+                         imageStyle={{ opacity:0.3}} 
+                         style={{padding:30, height: '100%', resizeMode: 'cover' }}>
+        <View style={{ flex:1, marginHorizontal: 22 }}>
+            <View style={{ marginVertical: 22 }}>
+                <Text style={{
+                    fontSize: 24, fontWeight: "bold", marginVertical:12, color: COLORS.black
+                }}> Bienvenido a </Text>
+                <Text style={{fontSize:14, color: COLORS.black}}>
+                    Gestión de Transporte y Carga
+                </Text>
+                <Text style={{fontSize:14, color: COLORS.black}}>
+                    ingrese sus datos
+                </Text>
+            </View>
+
+            <View style={{ marginBottom: 12}}>
+                <Text style={{
+                    fontSize:16,
+                    fontWeight: 400,
+                    marginVertical: 8
+                    }}> Email </Text>
+                <View style={{
+                    width:'100%',
+                    height: 48,
+                    borderColor: COLORS.black,
+                    borderWidth: 1,
+                    borderRadius: 8,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    paddingLeft:22
+                }}>
+                    <TextInput placeholder='Ingrese su email' 
+                               placeholderTextColor={COLORS.black} 
+                               keyboardType='email-address'
+                               style={{ width: "100%" 
+
+                               }}/>
+                </View>
+            </View>
+
+            <View style={{ marginBottom: 12}}>
+                <Text style={{
+                    fontSize:16,
+                    fontWeight: 400,
+                    marginVertical: 8
+                    }}> Contraseña </Text>
+                <View style={{
+                    width:'100%',
+                    height: 48,
+                    borderColor: COLORS.black,
+                    borderWidth: 1,
+                    borderRadius: 8,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    paddingLeft:22
+                }}>
+                    <TextInput placeholder='Ingrese una contraseña' 
+                               placeholderTextColor={COLORS.black}                                
+                               style={{ width: "100%" 
+
+                               }} 
+                               secureTextEntry={isPasswordShow}/>
+                    <TouchableOpacity onPress={()=> setIsPasswordShow(!isPasswordShow)}  
+                                      style={{
+                                            position: "absolute",
+                                            right: 12
+                                        }}> 
+                       { 
+                            isPasswordShow == true ? (
+                                <Ionicons  name="eye-off" size={24} color={COLORS.black} />
+                            ) : (
+                                <Ionicons  name="eye" size={24} color={COLORS.black} />
+                            )
+                       }
+                        
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+            <Button title="Iniciar" filled style={{
+                marginTop:25,
+                marginBottom: 10
+            }}/>
+
+            <View style={{flexDirection: 'row', 
+                          alignItems: 'center',
+                          marginVertical: 20
+            }}>
+
+                <View style={{ flex:1, height: 1, backgroundColor: COLORS.grey, marginHorizontal: 10 }} />
+                <Text style={{ fontSize: 14 }}> o inicia con </Text>
+                <View style={{ flex:1, height: 1, backgroundColor: COLORS.grey, marginHorizontal: 10 }} />
+            </View>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                <TouchableOpacity onPress={ () => console.log('Prossed')} 
+                                  style={{ flex:1,
+                                           alignItems: 'center',
+                                           justifyContent: 'center',
+                                           flexDirection: 'row',
+                                           height: 52,
+                                           borderWidth: 1, 
+                                           borderColor: COLORS.black,
+                                           marginRight: 4, 
+                                           borderRadius: 10
+
+                                  }}>
+                    <Image source={require('../assets/Google__G__logo.svg.png')} 
+                           style={{
+                                height: 36,
+                                width: 36,
+                                marginRight: 8
+                           }}
+                           resizeMode='contain'
+                           />
+                        <Text>Google</Text>
+
+                </TouchableOpacity>
+            </View>
+
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                marginVertical: 22
+            }}>
+                <Text style={{fontSize:16, color:COLORS.black}}>Crear una </Text>
+                <Pressable onPress={()=> navigation.navigate("Signup")}>
+                    <Text style={{ fontSize:16, color: COLORS.primary, fontWeight:'bold', marginLeft:6 }}> Cuenta</Text>  
+                </Pressable>
+            </View>
+
         </View>
+        </ImageBackground>
+        
+      </SafeAreaView>
     )
 }
 
--- screens/Signup.js
+++ screens/Signup.js
@@ -1,4 +1,4 @@
-import  { View, Text, TextInput } from 'react-native';
+import  { View, Text, TextInput, Image, Pressable, ImageBackground, StatusBar } from 'react-native';
 import React , {useState} from 'react';
 import {SafeAreaView } from 'react-native-safe-area-context'
 import COLORS from '../constants/colors';
@@ -11,47 +11,50 @@ const Signup = ({navigation}) => {
     const [isPasswordShow, setIsPasswordShow] = useState(false);
     const [isChecked, setIsChecked] = useState(false);
     return (
-      <SafeAreaView style={{flex:1, backgroundColor: COLORS.white}}>
-        <View style={{ flex:1, marginHorizontal: 22 }}>
-            <View style={{ marginVertical: 22 }}>
-                <Text style={{
-                    fontSize: 24, fontWeight: "bold", marginVertical:12, color: COLORS.black
-                }}> Crear Cuenta </Text>
-                <Text style={{fontSize:14, color: COLORS.black}}>
-                    Gestión de Transporte y Carga
-                </Text>
-            </View>
-
-            <View style={{ marginBottom: 12}}>
-                <Text style={{
-                    fontSize:16,
-                    fontWeight: 400,
-                    marginVertical: 8
-                    }}> Email </Text>
-                <View style={{
-                    width:'100%',
-                    height: 48,
-                    borderColor: COLORS.black,
-                    borderWidth: 1,
-                    borderRadius: 8,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    paddingLeft:22
-                }}>
-                    <TextInput placeholder='Ingrese su email' 
-                               placeholderTextColor={COLORS.black} 
-                               keyboardType='email-address'
-                               style={{ width: "100%" 
-
-                               }}/>
+        
+      <SafeAreaView style={{flex:1, backgroundColor: COLORS.white }}>
+        <StatusBar />
+        <ImageBackground source={require('../assets/camion-coronel.jpeg')} imageStyle={{ opacity:0.3}} style={{padding:20, height: '100%', resizeMode: 'cover' }}>
+            <View style={{ flex:1, marginHorizontal: 22 }}>
+                <View style={{ marginVertical: 22 }}>
+                    <Text style={{
+                        fontSize: 24, fontWeight: "bold", marginVertical:12, color: COLORS.black
+                    }}> Crear Cuenta </Text>
+                    <Text style={{fontSize:14, color: COLORS.black}}>
+                        Gestión de Transporte y Carga
+                    </Text>
                 </View>
-            </View>
 
-            <View style={{ marginBottom: 12}}>
-                <Text style={{
+                <View style={{ marginBottom: 12}}>
+                    <Text style={{
                         fontSize:16,
                         fontWeight: 400,
                         marginVertical: 8
+                        }}> Email </Text>
+                    <View style={{
+                        width:'100%',
+                        height: 48,
+                        borderColor: COLORS.black,
+                        borderWidth: 1,
+                        borderRadius: 8,
+                        alignItems: "center",
+                        justifyContent: "center",
+                        paddingLeft:22
+                    }}>
+                        <TextInput placeholder='Ingrese su email' 
+                                placeholderTextColor={COLORS.black} 
+                                keyboardType='email-address'
+                                style={{ width: "100%" 
+
+                                }}/>
+                    </View>
+                </View>
+
+                <View style={{ marginBottom: 12}}>
+                    <Text style={{
+                            fontSize:16,
+                            fontWeight: 400,
+                            marginVertical: 8
                         }}> N° Movil </Text>
                 <View style={{
                     width:'100%',
@@ -122,44 +125,7 @@ const Signup = ({navigation}) => {
                 </View>
             </View>
 
-            <View style={{ marginBottom: 12}}>
-                <Text style={{
-                    fontSize:16,
-                    fontWeight: 400,
-                    marginVertical: 8
-                    }}> Confirma Contraseña </Text>
-                <View style={{
-                    width:'100%',
-                    height: 48,
-                    borderColor: COLORS.black,
-                    borderWidth: 1,
-                    borderRadius: 8,
-                    alignItems: "center",
-                    justifyContent: "center",
-                    paddingLeft:22
-                }}>
-                    <TextInput placeholder='Confirme la contraseña' 
-                               placeholderTextColor={COLORS.black}                                
-                               style={{ width: "100%" 
-
-                               }} 
-                               secureTextEntry={isPasswordShow}/>
-                    <TouchableOpacity onPress={()=> setIsPasswordShow(!isPasswordShow)}  
-                                      style={{
-                                            position: "absolute",
-                                            right: 12
-                                        }}> 
-                       { 
-                            isPasswordShow == true ? (
-                                <Ionicons  name="eye-off" size={24} color={COLORS.black} />
-                            ) : (
-                                <Ionicons  name="eye" size={24} color={COLORS.black} />
-                            )
-                       }
-                        
-                    </TouchableOpacity>
-                </View>
-            </View>
+         
 
             <View style={{ 
                 flexDirection: 'row',
@@ -176,17 +142,61 @@ const Signup = ({navigation}) => {
 
             </View>
 
-            <Button title="Siguiente" filled style={{
+            <Button title="Crear" filled style={{
                 marginTop:25,
                 marginBottom: 10
             }}/>
 
-            <Button onPress={()=>navigation.navigate("welcome")}
-                          title= "volver" 
-                          style={{marginTop:30, width:"100%"}}  
-                          />
+           
+            <View style={{flexDirection: 'row', 
+                          alignItems: 'center',
+                          marginVertical: 20
+            }}>
 
+                <View style={{ flex:1, height: 1, backgroundColor: COLORS.grey, marginHorizontal: 10 }} />
+                <Text style={{ fontSize: 14 }}> o crear cuenta con </Text>
+                <View style={{ flex:1, height: 1, backgroundColor: COLORS.grey, marginHorizontal: 10 }} />
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                <TouchableOpacity onPress={ () => console.log('Prossed')} 
+                                  style={{ flex:1,
+                                           alignItems: 'center',
+                                           justifyContent: 'center',
+                                           flexDirection: 'row',
+                                           height: 52,
+                                           borderWidth: 1, 
+                                           color: COLORS.white,
+                                           borderColor: COLORS.black,
+                                           marginRight: 4, 
+                                           borderRadius: 10
+
+                                  }}>
+                    <Image source={require('../assets/Google__G__logo.svg.png')} 
+                           style={{
+                                height: 36,
+                                width: 36,
+                                marginRight: 8,
+                    
+                           }}
+                           resizeMode='contain'
+                           />
+                        <Text>Google</Text>
+
+                </TouchableOpacity>
+            </View>
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                marginVertical: 22
+            }}>
+                <Text style={{fontSize:16, color:COLORS.black}}> Ya tengo una cuenta </Text>
+                <Pressable onPress={()=> navigation.navigate("Login")}>
+                    <Text style={{ fontSize:16, color: COLORS.primary, fontWeight:'bold', marginLeft:6 }}>Login</Text>  
+                </Pressable>
+            </View>
         </View>
+        </ImageBackground>
+        
       </SafeAreaView>
     )
 }
